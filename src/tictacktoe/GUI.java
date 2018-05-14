7ac8c65e5dc8226ebf1ce9c9de54d72be3f43a1d
BertrandPennyworth <39150101+BertrandPennyworth@users.noreply.github.com>
2018-05-14 19:20:08 +0000

    Update GUI.java

--- src/tictacktoe/GUI.java
+++ src/tictacktoe/GUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictacktoe;
+
 
 import java.awt.BorderLayout;
 import java.awt.Component;
@@ -25,15 +25,7 @@ import javax.swing.JPanel;
 public class GUI  extends JFrame {
     
     private JPanel panel ;
-    private JButton btn1 ; 
-    private JButton btn2 ; 
-    private JButton btn3 ; 
-    private JButton btn4 ; 
-    private JButton btn5 ; 
-    private JButton btn6 ; 
-    private JButton btn7 ; 
-    private JButton btn8 ; 
-    private JButton btn9 ; 
+    private JButton btn;
     private JLabel label ; 
     private JLabel label2 ; 
     private JPanel panel2;
@@ -45,24 +37,14 @@ public class GUI  extends JFrame {
         label = new JLabel ("You Won");
         label2 = new JLabel ("Second Label ");
         panel.setPreferredSize(new Dimension(400,400));
-        btn1 = new JButton();
-        btn2 = new JButton();
-        btn3 = new JButton();
-        btn4 = new JButton();
-        btn5 = new JButton();
-        btn6 = new JButton();
-        btn7 = new JButton();
-        btn8 = new JButton();
-        btn9 = new JButton();
-        panel.add(btn1);
-        panel.add(btn2);
-        panel.add(btn3);
-        panel.add(btn4);
-        panel.add(btn5);
-        panel.add(btn6);
-        panel.add(btn7);
-        panel.add(btn8);
-        panel.add(btn9);
+        for(int i=0; i<9; i++){
+            btn = new JButton();
+            panel.add(btn);
+        }
+        
+
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label2.setHorizontalAlignment(JLabel.CENTER);
         add(panel, BorderLayout.CENTER);
         add(label,BorderLayout.NORTH);
         add(label2,BorderLayout.SOUTH);
