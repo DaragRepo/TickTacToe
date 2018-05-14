aa94cc4ebef71e5b3c429a07df413b629e07180a
Darag1996 <moha.darag@yahoo.com>
2018-05-14 19:38:20 +0200

    GUI

--- src/tictacktoe/GUI.java
+++ src/tictacktoe/GUI.java
@@ -6,13 +6,16 @@
 package tictacktoe;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -31,13 +34,16 @@ public class GUI  extends JFrame {
     private JButton btn7 ; 
     private JButton btn8 ; 
     private JButton btn9 ; 
-    
-    
+    private JLabel label ; 
+    private JLabel label2 ; 
+    private JPanel panel2;
     public GUI () {
         
         setSize(500,500);
         setTitle("Tic Tack Toe Game");
         panel = new JPanel(new GridLayout(3,3));
+        label = new JLabel ("You Won");
+        label2 = new JLabel ("Second Label ");
         panel.setPreferredSize(new Dimension(400,400));
         btn1 = new JButton();
         btn2 = new JButton();
@@ -57,8 +63,11 @@ public class GUI  extends JFrame {
         panel.add(btn7);
         panel.add(btn8);
         panel.add(btn9);
-        add(panel);
-        setLayout(new FlowLayout());
+        add(panel, BorderLayout.CENTER);
+        add(label,BorderLayout.NORTH);
+        add(label2,BorderLayout.SOUTH);
+//        panel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+//        setLayout(new FlowLayout());
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
