c9e9e431709090ea8dce49ea2697427eba10d129
Darag1996 <moha.darag@yahoo.com>
2018-05-14 05:31:44 +0200

    GUI

---
+++ src/tictacktoe/GUI.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictacktoe;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author moh
+ */
+public class GUI  extends JFrame {
+    
+    private JPanel panel ;
+    private JButton btn1 ; 
+    private JButton btn2 ; 
+    private JButton btn3 ; 
+    private JButton btn4 ; 
+    private JButton btn5 ; 
+    private JButton btn6 ; 
+    private JButton btn7 ; 
+    private JButton btn8 ; 
+    private JButton btn9 ; 
+    
+    
+    public GUI () {
+        
+        setSize(400,400);
+        setTitle("Tic Tack Toe Game");
+        panel = new JPanel(new GridLayout(3,3));
+        panel.setPreferredSize(new Dimension(100, 100));
+        btn1 = new JButton();
+        btn2 = new JButton();
+        btn3 = new JButton();
+        btn4 = new JButton();
+        btn5 = new JButton();
+        btn6 = new JButton();
+        btn7 = new JButton();
+        btn8 = new JButton();
+        btn9 = new JButton();
+        btn9.setText("X");
+        panel.add(btn1);
+        panel.add(btn2);
+        panel.add(btn3);
+        panel.add(btn4);
+        panel.add(btn5);
+        panel.add(btn6);
+        panel.add(btn7);
+        panel.add(btn8);
+        panel.add(btn9);
+        add(panel);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    
+    
+    
+    
+}
