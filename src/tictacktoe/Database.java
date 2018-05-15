3d8e874946212ab320a904716cb8e3decdc51684
Darag1996 <moha.darag@yahoo.com>
2018-05-15 17:15:18 +0200

    Model

--- src/tictacktoe/Database.java
+++ src/tictacktoe/Database.java
@@ -7,94 +7,30 @@ package tictacktoe;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
  * @author moh
  */
 public class Database {
-
+    
     private String db_url;
     private String db_user;
     private String db_pass;
     private Connection conn;
-
-    public Database(String db_url, String db_user, String db_pass) throws SQLException {
+    private String table_name;
+    // establish the Database Connection 
+    public Database(String db_url, String db_user, String db_pass,String table_name) throws SQLException {
         this.db_url = db_url;
         this.db_user = db_user;
         this.db_pass = db_pass;
+        this.table_name = table_name;
         this.conn = DriverManager.getConnection(this.db_url, this.db_user, this.db_pass);
     }
-
-    public void insert(String name) {
-
-        try {
-
-            String sql = "INSERT INTO employee (name) VALUES (?)";
-
-            PreparedStatement statement = this.conn.prepareStatement(sql);
-            statement.setString(1, name);
-
-            String sql2 = "SELECT name FROM employee WHERE name=?";
-
-            PreparedStatement ps = conn.prepareStatement(sql2);
-            ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
-            if (!rs.isBeforeFirst()) {
-                int rowsInserted = statement.executeUpdate();
-                if (rowsInserted > 0) {
-                    System.out.println("A new Record was inserted successfully!");
-
-                }
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-    public String select() throws SQLException {
-        String sql = "SELECT name FROM employee";
-
-        Statement statement = this.conn.createStatement();
-        ResultSet result = statement.executeQuery(sql);
-        String data = "";
-        while (result.next()) {
-            String name = result.getString(1);
-            data += "Employee Name : " + name + "\n";
-
-        }
-        return data;
-    }
-
-    public void update(String name) throws SQLException {
-        String sql = "UPDATE employee SET name=? WHERE id=?";
-
-        PreparedStatement statement = this.conn.prepareStatement(sql);
-        statement.setString(1, name);
-
-        int rowsUpdated = statement.executeUpdate();
-        if (rowsUpdated > 0) {
-            System.out.println("An existing user was updated successfully!");
-        }
-    }
-
-    public void delete(int id) throws SQLException {
-        String sql = "DELETE FROM employee WHERE id=?";
-
-        PreparedStatement statement = this.conn.prepareStatement(sql);
-        statement.setString(1, String.valueOf(id));
-
-        int rowsDeleted = statement.executeUpdate();
-        if (rowsDeleted > 0) {
-            System.out.println("A user was deleted successfully!");
-        }
-
-    }
-
+    
+    // insert into the Database 
+    
+    
+    
 }
--- src/tictacktoe/TicTackToe.java
+++ src/tictacktoe/TicTackToe.java
@@ -14,8 +14,17 @@ public class TicTackToe {
     /**
      * @param args the command line arguments
      */
+    public static void test (String... s) {
+       for (int i = 0 ; i < s.length ; i++ ){
+           System.out.println(s[i]);
+       }
+    }
+    
+    
     public static void main(String[] args) {
-        Login login = new Login ();
+//        Login login = new Login ();
+        test("1","2","3","4");
+        
 //        Register reg = new Register ();
 //        GUI gui = new GUI();
     }
